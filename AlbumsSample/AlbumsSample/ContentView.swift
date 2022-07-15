fa7a78f45fdb1137fb6dbf503abc49df28ab0337
posite <su84053@gmail.com>
2022-07-15 11:06:16 +0900

    album 가져온거 text로 보기

--- AlbumsSample/AlbumsSample/ContentView.swift
+++ AlbumsSample/AlbumsSample/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var albumStore = AlbumStore.singleton
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear{
-                AlbumStore.singleton.load()
-            }
+        NavigationView{
+            List{
+                ForEach(albumStore.albums, id:\.albumTitle){album in
+                    NavigationLink(destination: Text("\(album.albumTitle)")) {
+                        AlbumItemView(album: album)
+                    }
+                }
+                
+            }.navigationTitle(Text("Albums"))
+        }
+        .onAppear{albumStore.load()}
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AlbumItemView: View {
+    static let imageSize = 50
+    let album : Album
+    var body: some View {
+        HStack{
+            Image(systemName: "music.note")
+                .resizable()
+                .frame(width: CGFloat(AlbumItemView.imageSize),
+                       height: CGFloat(AlbumItemView.imageSize))
+            VStack(alignment: .leading){
+                Text("\(album.albumTitle)")
+                    .font(.title2)
+                Text("\(album.artistName)")
+                    .font(.footnote)
+                    .foregroundColor(.purple)
+            }
+        }
     }
 }
